5281635030eb76bdd536fee17558920bda064e03
LogicAL007 <ayomidemtsh@gmail.com>
2023-11-28 10:24:35 +0100

    restructuring and using poetry to manage dependencies

--- Document_store/document_store/API-Ingest/app/main.py
+++ Document_store/document_store/API-Ingest/app/main.py
@@ -1,23 +1,11 @@
-# You need this to use FastAPI, work with statuses and be able to end HTTPExceptions
-from fastapi import FastAPI, status, HTTPException
- 
-# You need this to be able to turn classes into JSONs and return
+from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-
-# Needed for json.dumps
 import json
-
-# Both used for BaseModel
 from pydantic import BaseModel
-
 from datetime import datetime
-from kafka import KafkaProducer, producer
+from kafka import KafkaProducer
 
-
-
-# Create class (schema) for the JSON
-# Date get's ingested as string and then before writing validated
 class InvoiceItem(BaseModel):
     InvoiceNo: int
     StockCode: str
@@ -28,32 +16,24 @@ class InvoiceItem(BaseModel):
     CustomerID: int
     Country: str
 
-# This is important for general execution and the docker later
 app = FastAPI()
 
-# Base URL
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-# Add a new invoice
 @app.post("/invoiceitem")
-async def post_invoice_item(item: InvoiceItem): #body awaits a json with invoice item information
+async def post_invoice_item(item: InvoiceItem):
     print("Message received")
     try:
-        # Evaluate the timestamp and parse it to datetime object you can work with
+        # change the timestamp format
         date = datetime.strptime(item.InvoiceDate, "%d/%m/%Y %H:%M")
-
-        print('Found a timestamp: ', date)
-
-        # Replace strange date with new datetime
-        # Use strftime to parse the string in the right format (replace / with - and add seconds)
         item.InvoiceDate = date.strftime("%d-%m-%Y %H:%M:%S")
         print("New item date:", item.InvoiceDate)
         
         # Parse item back to json
         json_of_item = jsonable_encoder(item)
-        
+
         # Dump the json out as string
         json_as_string = json.dumps(json_of_item)
         print(json_as_string)
@@ -63,17 +43,11 @@ async def post_invoice_item(item: InvoiceItem): #body awaits a json with invoice
 
         # Encode the created customer item if successful into a JSON and return it to the client with 201
         return JSONResponse(content=json_of_item, status_code=201)
-    
-    # Will be thrown by datetime if the date does not fit
-    # All other value errors are automatically taken care of because of the InvoiceItem Class
     except ValueError:
         return JSONResponse(content=jsonable_encoder(item), status_code=400)
         
 
 def produce_kafka_string(json_as_string):
-    # Create producer
         producer = KafkaProducer(bootstrap_servers='kafka:9092',acks=1)
-        
-        # Write the string as bytes because Kafka needs it this way
         producer.send('ingestion-topic', bytes(json_as_string, 'utf-8'))
         producer.flush()
---
+++ Document_store/document_store/client/api-client.py
@@ -0,0 +1,42 @@
+import linecache
+import json
+import requests
+import logging
+import numpy as np
+from numpy import add
+import pandas as pd
+
+df = pd.read_csv ('/mnt/c/Users/ayomi/OneDrive/Documents/GitHub/E-commmerce-streaming-project/data.csv', encoding= 'ISO-8859-1') 
+
+def convert_csv_to_txt(df):
+    df['json'] = df.to_json(orient='records', lines=True).splitlines()
+    dfjson = df['json']
+    np.savetxt(r'./output.txt', dfjson.values, fmt='%s')
+    return 
+
+
+def request_post(file_path, start=1, end=10):
+    '''
+    Sends POST requests to a specified URL with JSON data read from a file.
+
+    :Param file_path: The path to the file from which to read the JSON strings.
+    :Param start: The starting line number in the file to read from. Defaults to 1.
+    :Param end: The ending line number in the file to read up to. Defaults to 10.
+
+    Returns:
+    None: The function does not return any value.
+    '''
+    i = start
+    while i <= end:
+        line = linecache.getline(file_path, i)
+        if line.strip():
+            myjson = json.loads(line)
+            response = requests.post('http://localhost:80/invoiceitem', json=myjson)
+            logging.info(response.json())
+        i += 1
+    return
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    convert_csv_to_txt(df)
+    request_post('./output.txt')
